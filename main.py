4f59d184008f3f005feccb3024f08b70e4df982d
Kareem Mohamed <kareemmagdy420@gmail.com>
2025-03-09 21:33:56 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import pickle
+import pandas as pd
 from math import inf
 from typing import List
 import numpy as np
@@ -9,19 +10,18 @@ from commonroad_utils.parser.scenario import get_scenario
 from commonroad_utils.parser.parser import Parser
 from commonroad.common.util import Interval
 from commonroad.scenario.state import ExtendedPMState, PMState
-from commonroad_utils.parser.utils import create_trajectory_from_list_states, visualize_solution, visualize_scenario, create_video
+from commonroad_utils.parser.utils import *
 # import sys
 # sys.path.append('/home/wenguang/workplace/my-frenet/frenet_optimal_trajectory_planner/FrenetOptimalTrajectory')
 
 os.environ["SHOW_SAMPLING_PATH"] = '1'
 
 scenario_path = os.getcwd() + '/commonroad_utils/Critical_Transformed/'
-scenario_name = 'DEU_Lohmar-47_1_T-1.xml'
+scenario_name = 'PRI_Barceloneta-4_5_T-1.xml'
 # scenario_name = 'USA_US101-28_1_T-1.xml'
 
 scenario, planning_problem, pp_set = get_scenario(scenario_path, scenario_name)
 
-# planning_problem.goal.state_list[0].position.center = np.array([2.0, -12.0])
 # Get the initial and goal positions
 start_pos = planning_problem.initial_state.position
 goal_pos = planning_problem.goal.state_list[0].position.center
@@ -35,24 +35,29 @@ initial_vel_y = inital_vel * np.sin(initial_orientation)
 parser = Parser(scenario=scenario, planning_problem=planning_problem,\
                   x_interval=Interval(-inf, inf), y_interval=Interval(-inf, inf))
 
-EPS = 1.0           # Epsilon of reached goal comparison
+EPS = 1.5           # Epsilon of reached goal comparison
 LEN_DRAW = 10     # The length of the drawn trajectory (Number of states)
+
 SAVE = False
-LOAD = False
+SAVE_CSV = False
+LOAD = True
+SAVE_PROFILES = False
+CREATE_VIDEO = False
+
 # print(planning_problem.initial_state.acceleration)
 conds = {
       's0': parser.parse_initial_position(x_only=True),
-      'target_speed': 10.0,
+      'target_speed': 15.0,
       # 'target_speed': planner.x_0.velocity,  # Uncomment to parsing the target speed from the scenario
       'acc': planning_problem.initial_state.acceleration,
       'wp': parser.parse_waypoints(initial_state=start_pos, goal_state=goal_pos),
       'obs': parser.parse_obstacles(time_step=0),
       'pos': parser.parse_initial_position(x_only=False),
       'vel': [initial_vel_x, initial_vel_y], # Velocity in X and Y directions
-      # 'vel': [8.0, 0.2], # Velocity in X and Y directions
+      # 'vel': [-1.0, -12.0], # Velocity in X and Y directions
 }
 
-print(initial_vel_x, initial_vel_y)
+# print(initial_vel_x, initial_vel_y)
 
 initial_conditions = {
       'ps': conds['s0'],
@@ -72,26 +77,31 @@ if SAVE:
 if LOAD:
       with open(f'commonroad_utils/frenet_ic/{scenario_name[:-4]}.pkl', 'rb') as f:
             initial_conditions = pickle.load(f)
+            
+ic_df = pd.DataFrame({
+      'name': ['target_speed', 'vel_x', 'vel_y'],
+      'value': [initial_conditions['target_speed'], initial_conditions['vel'][0], initial_conditions['vel'][1]]
+})
 
 hyperparameters = {
-      "max_speed": 15.0,
+      "max_speed": 40.0,
       "max_accel": 15.0,
-      "max_curvature": 100.0,
+      "max_curvature": 10.0,
       "max_road_width_l": 1.75,
       "max_road_width_r": 1.75,
       "d_road_w": 0.2,
-      "dt": 0.2,
+      "dt": 0.1,
       "maxt": 2.0,
       "mint": 1.0,
-      "d_t_s": 0.2,
-      "n_s_sample": 10.0,
+      "d_t_s": 0.1,
+      "n_s_sample": 100.0,
       "obstacle_clearance": -0.5,
-      "kd": 0.1,
+      "kd": 0.02,
       "kv": 0.1,
       "ka": 0.1,
-      "kj": 0.1,
+      "kj": 0.0,
       "kt": 0.1,
-      "ko": 0.1,
+      "ko": 190.0,
       "klat": 1.0,
       "klon": 1.0,
       "num_threads": 0
@@ -104,12 +114,24 @@ if SAVE:
 if LOAD:
       with open(f'commonroad_utils/frenet_hp/{scenario_name[:-4]}.pkl', 'rb') as f:
             hyperparameters = pickle.load(f)
+            
+hp_df = pd.DataFrame(list(hyperparameters.items()), columns=['name', 'value'])
+ic_df = pd.concat([ic_df, hp_df], ignore_index=True)
+
+# if SAVE_CSV:
+#       ic_df.to_csv(f'commonroad_utils/frenet_hp/{scenario_name[:-4]}.csv', index=False)
+
+print(hyperparameters)
 
 wp = initial_conditions["wp"]
-# print(wp)
+# print(wp[-1])
 
 print(os.getpid())
 acc_states = []
+velocities_ = []
+accelerations_ = []
+sampled_paths_ = []
+full_trajectories_ = []
 fig, ax = plt.subplots(figsize=(25, 10))
 
 show_sampling_path = os.environ.get("SHOW_SAMPLING_PATH", False)
@@ -129,7 +151,7 @@ for i in range(200):
             result_x, result_y, speeds, accelerations, ix, iy, iyaw, d, s, speeds_x, \
                   speeds_y, misc, costs, success, runtime = \
                   fot_wrapper.run_fot(initial_conditions, hyperparameters)
-      print(accelerations[1])
+      # print(speeds[1])
       states_list: List[List[ExtendedPMState]] = [[ExtendedPMState(time_step=i+j, position=np.array([result_x[j], result_y[j]]),\
                                                 velocity=speeds[j], orientation=iyaw[j], acceleration=accelerations[j])] for j in range(len(result_x[:LEN_DRAW]))]
       # Create PMState for each sample path
@@ -142,6 +164,7 @@ for i in range(200):
             
       # Create trajectories from the sampling states
       sampling_paths = [create_trajectory_from_list_states(path_states) for path_states in sampling_states if path_states]
+      sampled_paths_.append(sampling_paths)
       
       # Convert the list of PMstates to CommonRoad trajectory (full trajectory to draw it and a shorter one to excute it)
       try:
@@ -168,11 +191,12 @@ for i in range(200):
                         full_trajectory = full_trajectory,
                         waypoints = wp, 
                         t_s = i,
-                        fig=fig,
                         ax=ax)
       
       # print("Euclidean Distance: ", np.linalg.norm(np.array([result_x[1], result_y[1]]) - goal_pos))
-
+      acc_states.append(states_list[1][0])
+      full_trajectories_.append(full_trajectory)
+      
       if success:
             # If planning suceeded, check if goal was reached. If true, break the planning loop
             if np.linalg.norm(np.array([result_x[1], result_y[1]]) - goal_pos) < EPS:
@@ -189,17 +213,27 @@ for i in range(200):
             initial_conditions['acc'] = np.array(accelerations[1])
             # initial_conditions['obs'] = np.array([])      # Uncomment to test with no obstacles
             initial_conditions['obs'] = np.array(parser.parse_obstacles(time_step=i+1))         # Comment to test with no obstacles
-            acc_states.append(states_list[1][0])
+            
+            velocities_.append(speeds[1])
+            accelerations_.append(accelerations[1])
       else:
             # continue
             print("Failed unexpectedly")
             break
       
 # print(acc_states)
-# excuted_trajectory = create_trajectory_from_list_states([acc_states])
-# create_video(
-#       scenario=scenario,
-#       planning_problem_set=planning_problem,
-#       excuted_trajectory=excuted_trajectory,
-#       waypoints=wp
-# )
+excuted_trajectory = create_trajectory_from_list_states([acc_states])
+
+if CREATE_VIDEO:
+      create_video(
+            scenario=scenario,
+            planning_problem_set=planning_problem,
+            excuted_trajectory=excuted_trajectory,
+            full_trajectory=full_trajectories_,
+            sampled_trajectories=sampled_paths_,
+            waypoints=wp
+      )
+
+if SAVE_PROFILES:
+      plot_profile(velocities_, 'Velocity', scenario_name, SAVE_PROFILES)
+      plot_profile(accelerations_, 'Acceleration', scenario_name, SAVE_PROFILES)
