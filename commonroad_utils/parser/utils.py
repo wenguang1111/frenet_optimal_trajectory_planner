0131c6db4573faf4c9a7d6b1a39cedb5aed5914d
Kareem Mohamed <kareemmagdy420@gmail.com>
2025-03-09 20:59:49 +0100

    added function to plot profiles and modified create_video to include sampled paths

--- commonroad_utils/parser/utils.py
+++ commonroad_utils/parser/utils.py
@@ -2,6 +2,7 @@ from typing import List
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+import os
 from commonroad.geometry.shape import Rectangle
 from commonroad.planning.planning_problem import PlanningProblemSet
 from commonroad.prediction.prediction import TrajectoryPrediction
@@ -57,7 +58,6 @@ def visualize_solution(
     full_trajectory: Trajectory,
     waypoints, 
     t_s,
-    fig,
     ax,
     drawn_trajectories: List[Trajectory] = None, 
     obstacles = None
@@ -117,7 +117,6 @@ def visualize_solution(
     # Loop on the number of time steps in the excuted trajectory
     for i in range(0, num_time_steps):
         ax.cla()
-        # display.clear_output(wait=True)
         
         renderer = MPRenderer(ax=ax)
         renderer.ax = ax
@@ -172,12 +171,26 @@ def visualize_solution(
         plt.gca().set_aspect("equal")
         plt.show()
         
-# TODO: Modify the function to take the sampled trajectories
+        
 def create_video(
     scenario: Scenario, 
     planning_problem_set: PlanningProblemSet, 
     excuted_trajectory: Trajectory,
+    full_trajectory: List[Trajectory],
+    sampled_trajectories: List[List[Trajectory]],
     waypoints):
+    
+    '''
+    Creates a video of the scenario, planning problem, waypoints, and the excuted trajectory.
+    
+    args:
+        scenario: Scenario object.
+        planning_problem_set: PlanningProblemSet object.
+        excuted_trajectory: Trajectory object.
+        full_trajectory: Trajectory object.
+        sampled_trajectories: List of lists of Trajectory objects.
+        waypoints: List of waypoints.
+    '''
     
     num_time_steps = len(excuted_trajectory.state_list)
     
@@ -213,63 +226,46 @@ def create_video(
     # Initialize the vehicle and trajectory drawing parameters
     ego_params = DynamicObstacleParams()
     obs_params = DynamicObstacleParams()
-    # traj_params = TrajectoryParams()
-    # draw_params = MPDrawParams()
+    traj_params = TrajectoryParams()
+    sampled_traj_params = TrajectoryParams()
+
     ego_params.vehicle_shape.occupancy.shape.facecolor = "green"
     ego_params.draw_icon = True
     
     obs_params.vehicle_shape.occupancy.shape.facecolor = "blue"
     obs_params.draw_icon = True
     
+    traj_params.draw_trajectory = False
+    traj_params.facecolor = "#6aa84f"
+    traj_params.draw_continuous = True
+    traj_params.zorder = 60
+    traj_params.line_width = 2
+    
+    sampled_traj_params.draw_trajectory = True
+    sampled_traj_params.facecolor = "#808080"
+    sampled_traj_params.draw_continuous = True
+    sampled_traj_params.zorder = 60
+    sampled_traj_params.line_width = 2
+    
     # display.clear_output(wait=True)
     plt.figure(figsize=(25, 10))
     ax = plt.gca()
     
     renderer = MPRenderer()
-    # renderer.ax
     renderer.focus_obstacle_id = dynamic_obstacle_id
     renderer.draw_params.time_begin = excuted_trajectory.state_list[0].time_step
     renderer.draw_params.dynamic_obstacle.draw_shape = True
     renderer.draw_params.dynamic_obstacle.draw_icon = True
     
-    # scenario.draw(renderer)
-    # planning_problem_set.draw(renderer)
-    
-    # circles = [Circle(radius = 0.5, center = np.array(wp)) for wp in waypoints]
-    # for c in circles:
-    #     c.draw(renderer)
-    
-    # scenario.draw(renderer)
-    # planning_problem_set.draw(renderer)
-    
     def update(frame):
-        # renderer.clear()
-        # Drawing parameters of the excuted trajectory
-        # ego_params.time_begin = excuted_trajectory.state_list[0].time_step
-    
         ego_params.time_begin = frame
-        ego_params.trajectory.draw_trajectory = True
+        ego_params.trajectory.draw_trajectory = False
         ego_params.trajectory.facecolor = "#ff00ff"
-        ego_params.trajectory.draw_continuous = True
+        ego_params.trajectory.draw_continuous = False
         ego_params.trajectory.zorder = 60
-        ego_params.trajectory.line_width = 2
+        ego_params.trajectory.line_width = 4
         
         obs_params.time_begin = frame
-        # ego_params.trajectory.draw_trajectory = True
-        # ego_params.trajectory.facecolor = "#ff00ff"
-        # ego_params.trajectory.draw_continuous = True
-        # ego_params.trajectory.zorder = 60
-        # ego_params.trajectory.line_width = 2
-        
-        # Drawing parameters of the full trajectory
-        # traj_params.draw_trajectory = True
-        # traj_params.facecolor = "#6aa84f"
-        # traj_params.draw_continuous = True
-        # traj_params.zorder = 60
-        # traj_params.line_width = 2
-        
-        # Drawing and rendering
-        # drawn_trajectory.draw(renderer, draw_params=traj_params)
         
         scenario.lanelet_network.draw(renderer)
         planning_problem_set.draw(renderer)
@@ -283,6 +279,12 @@ def create_video(
         circles = [Circle(radius = 0.5, center = np.array(wp)) for wp in waypoints]
         for c in circles:
             c.draw(renderer)
+                    
+        if sampled_trajectories:
+            for sample in sampled_trajectories[frame]:
+                sample.draw(renderer, draw_params=sampled_traj_params)
+                
+        full_trajectory[frame].draw(renderer, draw_params=traj_params)
             
         ego_vehicle.draw(renderer, draw_params=ego_params)
         plt.gca().set_aspect("equal")
@@ -290,15 +292,11 @@ def create_video(
         
     anim = FuncAnimation(ax.figure, 
                          update, 
-                        #  frames=planning_problem_set.goal.state_list[0].time_step.end,
-                        frames=num_time_steps+1,
-                        # frames=5, 
-                        #  init_func=init_frame, 
-                         blit=False, 
-                         interval=100)
+                         frames=num_time_steps,
+                         interval=200)
     
-    # anim.save('/home/kareem/my-frenet/commonroad_utils/scenarios_videos/crashed/' + scenario.scenario_id.__str__() + '.mp4', dpi=250, writer='ffmpeg')
-    anim.save('/home/kareem/my-frenet/commonroad_utils/scenarios_videos/crashed/' + 'test' + '.mp4', dpi=250, writer='ffmpeg')
+    anim.save('/home/kareem/frenet_optimal_trajectory_planner/commonroad_utils/scenarios_videos/' + scenario.scenario_id.__str__() + '.mp4', dpi=250, writer='ffmpeg')
+    # anim.save('/home/kareem/frenet_optimal_trajectory_planner/commonroad_utils/scenarios_videos/crashed/' + 'test' + '.mp4', dpi=250, writer='ffmpeg')
         
 def visualize_scenario(
     scenario: Scenario, 
@@ -306,15 +304,12 @@ def visualize_scenario(
     waypoints = None, 
 ) -> None:
     """
-    Plots the scenario, planning problem, waypoints, Ego vehicle, excuted, and full trajectory.
+    Plots the scenario, planning problem, and waypoints.
     
     Args:
         Scenario object.
         Planning problem object.
-        The drawn trajectory.
-        The excuted trajectory.
-        List of waypoints.
-        The current time step.
+        Waypoints (Optional).
     """
 
     plt.figure(figsize=(25, 10))
@@ -331,3 +326,34 @@ def visualize_scenario(
     plt.gca().set_aspect("equal")
     renderer.render()
     plt.show()
+    
+def plot_profile(
+    attribute,
+    name,
+    scenario_name,
+    save
+):
+    """
+    Plots the profile of the attribute against time steps.
+    
+    Args:
+        The attribute to plot.
+        The name of the attribute
+        Scenario name.
+        Save the plot in the profiles folder.
+    """
+    
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(len(attribute)), attribute)
+    plt.xlabel('Time Steps')
+    plt.ylabel(f'{name}')
+    plt.title(f'{name} Profile')
+    
+    plt.grid(True)
+    
+    path = os.getcwd() + '/commonroad_utils/profiles/' + scenario_name
+    
+    if save:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        plt.savefig(path + f'/{name}_profile.png')
