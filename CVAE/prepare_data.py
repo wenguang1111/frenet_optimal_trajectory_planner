288b7cfcd39c6aa3a5390f9c4367a3065b52356d
Kareem Mohamed <kareemmagdy420@gmail.com>
2025-07-06 19:58:23 +0200

    fix scenarios path and delete failed scenarios from dictionary

--- CVAE/prepare_data.py
+++ CVAE/prepare_data.py
@@ -8,7 +8,7 @@ from commonroad.prediction.prediction import TrajectoryPrediction
 from commonroad_route_planner.route_planner import RoutePlanner
 from commonroad.geometry.shape import Rectangle
 from commonroad.common.file_writer import CommonRoadFileWriter, OverwriteExistingFile
-from utils import save_scenario_at_timestep
+from utils import save_scenario_at_timestep, remove_entries_for_scenario
 
 import sys
 sys.path.insert(0, "/home/kareem/frenet_optimal_trajectory_planner/CVAE/commonroad-reactive-planner")
@@ -21,9 +21,9 @@ print(commonroad_rp.__file__)
 import matplotlib
 matplotlib.use('Agg')  # Non-GUI backend (renders to image files)
 
-dir = "/home/kareem/frenet_optimal_trajectory_planner/commonroad_utils/scenarios_w_goals"
+dir = "/home/kareem/frenet_optimal_trajectory_planner/CVAE/scenarios"
 config_file = "/home/kareem/frenet_optimal_trajectory_planner/CVAE/config/reactive_planner_config.yaml"
-save_dir = "/home/kareem/my-frenet/FrenetOptimalTrajectory/scenarios_modified"
+
 
 sampled_vars = {
       "scenario": [],
@@ -40,8 +40,6 @@ conditioned_vars = {
       "init_velocity": [],
       "goal_x": [],
       "goal_y": [],
-      # "reach_sets": [],
-      # "obstacles": [],
 }
 
 for sc in os.listdir(dir):
@@ -58,8 +56,6 @@ for sc in os.listdir(dir):
                   # run route planner
                   route_planner = RoutePlanner(config.scenario, config.planning_problem)
                   route = route_planner.plan_routes().retrieve_first_route()
-                  # print(config.planning_problem.initial_state.orientation)
-                  # print(config.planning_problem._goal_region.state_list[0].position.center)
                   
                   # Scenario has attribute position as a lanelet in goal (get the center of the lanelet) 
                   if isinstance(config.planning_problem.goal.state_list[0].position, \
@@ -120,57 +116,16 @@ for sc in os.listdir(dir):
                   if os.path.exists(img_save_dir) and os.path.isdir(img_save_dir):
                         os.rmdir(img_save_dir)
                         print(f"Deleted directory: {img_save_dir}")
+                        
+                  remove_entries_for_scenario(sampled_vars, sc[:-4])
+                  remove_entries_for_scenario(conditioned_vars, sc[:-4])
+
                   continue
             
 sampled_vars_df = pd.DataFrame(sampled_vars)
 sampled_vars_df.to_csv('data/sampled_vars.csv')
             
 conditioned_vars_df = pd.DataFrame(conditioned_vars)
-conditioned_vars.to_csv('data/conditioned_vars.csv')
+conditioned_vars_df.to_csv('data/conditioned_vars.csv')
                         
             
-            # ego_solution_trajectory = create_full_solution_trajectory(config, planner.record_state_list)
-            
-            # dynamic_obstacle_initial_state = InitialState(
-            #       position=config.planning_problem.initial_state.position,
-            #       orientation=config.planning_problem.initial_state.orientation,
-            #       velocity=config.planning_problem.initial_state.velocity,
-            #       time_step=config.planning_problem.initial_state.time_step,
-            #       yaw_rate=0,
-            #       slip_angle=0,
-            # )
-            
-            # dynamic_obstacle_shape = Rectangle(width=1.8, length=4.3)
-            # dynamic_obstacle_prediction = TrajectoryPrediction(
-            #       ego_solution_trajectory, dynamic_obstacle_shape
-            # )
-            
-            # # generate the dynamic obstacle according to the specification
-            # dynamic_obstacle_id = config.scenario.generate_object_id()
-            # dynamic_obstacle_type = ObstacleType.CAR
-            # ego_vehicle = DynamicObstacle(
-            #       dynamic_obstacle_id,
-            #       dynamic_obstacle_type,
-            #       dynamic_obstacle_shape,
-            #       dynamic_obstacle_initial_state,
-            #       dynamic_obstacle_prediction,
-            # )
-            
-            # config.scenario.add_objects(ego_vehicle)
-            
-            # fw = CommonRoadFileWriter(
-            #       config.scenario,
-            #       config.planning_problem_set,
-            #       config.scenario.author,
-            #       config.scenario.affiliation,
-            #       config.scenario.source,
-            #       config.scenario.tags
-            # )
-            
-#             fw.write_to_file(os.path.join(save_dir, sc), OverwriteExistingFile.ALWAYS)
-            
-#             scenario_ego_id['scenario'].append(sc)
-#             scenario_ego_id["ego_id"].append(dynamic_obstacle_id)
-
-# sc_ego_id_df = pd.DataFrame(scenario_ego_id)
-# sc_ego_id_df.to_csv('scenario_ego_id.csv')
--- CVAE/utils.py
+++ CVAE/utils.py
@@ -16,11 +16,11 @@ def save_scenario_at_timestep(
       
       Args:
             Scenario name.
-            Planning problem object.
+            Time step.
       """
       save_dir = "/home/kareem/frenet_optimal_trajectory_planner/CVAE/data/scenarios_imgs"
       sc_path = "/home/kareem/frenet_optimal_trajectory_planner/CVAE/scenarios"
-      scenario, _ = CommonRoadFileReader(os.path.join(sc_path, scenario_name) + ".xml").open()
+      scenario, pp = CommonRoadFileReader(os.path.join(sc_path, scenario_name) + ".xml").open()
       
       renderer = MPRenderer()
       renderer.draw_params.axis_visible = False
@@ -28,11 +28,25 @@ def save_scenario_at_timestep(
       renderer.draw_params.dynamic_obstacle.draw_shape = True
       renderer.draw_params.dynamic_obstacle.draw_icon = True
       scenario.draw(renderer)
+      pp.draw(renderer)
       
       plt.gca().set_aspect("equal")
       renderer.render()
       os.makedirs(save_dir + "/" + scenario_name, exist_ok=True)
       plt.savefig(save_dir + f"/{scenario_name}/time_step_{time_step}.png")
+      
+      
+def remove_entries_for_scenario(data_dict, scenario_name):
+    # Create a mask of indices to keep (where scenario is NOT the one to remove)
+    scenario_list = data_dict["scenario"]
+    indices_to_keep = [i for i, s in enumerate(scenario_list) if s != scenario_name]
+
+    # Apply the same filtering to every key
+    for key in data_dict:
+        data_dict[key] = [data_dict[key][i] for i in indices_to_keep]
+
+
     
     
-save_scenario_at_timestep("DEU_Flensburg-8_1_T-1", 0)
+if __name__ == "__main__":
+      save_scenario_at_timestep("DEU_Flensburg-10_1_T-1", 5)
