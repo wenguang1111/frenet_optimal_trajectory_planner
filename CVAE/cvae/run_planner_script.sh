393aec982deb1ef072aee317abc94abe3a79aa37
Kareem Mohamed <karem.mohamed@dlr.de>
2025-09-22 00:55:13 +0200

    option to choose device to run cvae on

--- CVAE/cvae/run_planner_script.sh
+++ CVAE/cvae/run_planner_script.sh
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64gb
-#SBATCH --time=11:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=none
 #SBATCH --mail-user=karem.mohamed@dlr.de
 
