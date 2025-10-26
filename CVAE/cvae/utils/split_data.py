25a2824453df1dbb9b8805f6c0a99049eeb86fd8
Kareem Mohamed <karem.mohamed@dlr.de>
2025-10-26 20:20:53 +0100

    update data splitting

--- CVAE/cvae/utils/split_data.py
+++ CVAE/cvae/utils/split_data.py
@@ -1,9 +1,13 @@
+import os
+import shutil
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+data_dir = "cvae/data/data_v2/"
+
 # Load full dataset
-c_df = pd.read_csv("cvae/data/data_extended/conditioned_vars_w_imgs.csv")
-x_df = pd.read_csv("cvae/data/data_extended/sampled_vars.csv")
+c_df = pd.read_parquet(data_dir + "conditioned_vars.parquet")
+x_df = pd.read_parquet(data_dir + "sampled_vars.parquet")
 
 scenarios = x_df["scenario"].unique()
 
@@ -19,10 +23,34 @@ c_train = c_df[c_df["scenario"].isin(train_sc)]
 c_val = c_df[c_df["scenario"].isin(val_sc)]
 c_test = c_df[c_df["scenario"].isin(test_sc)]
 
-x_train.to_csv("cvae/data/data_extended/x_train.csv", index=False)
-x_val.to_csv("cvae/data/data_extended/x_validation.csv", index=False)
-x_test.to_csv("cvae/data/data_extended/x_test.csv", index=False)
+x_train.to_parquet(data_dir + "x_train.parquet", index=False)
+x_val.to_parquet(data_dir + "x_validation.parquet", index=False)
+x_test.to_parquet(data_dir + "x_test.parquet", index=False)
 
-c_train.to_csv("cvae/data/data_extended/c_train.csv", index=False)
-c_val.to_csv("cvae/data/data_extended/c_validation.csv", index=False)
-c_test.to_csv("cvae/data/data_extended/c_test.csv", index=False)
+c_train.to_parquet(data_dir + "c_train.parquet", index=False)
+c_val.to_parquet(data_dir + "c_validation.parquet", index=False)
+c_test.to_parquet(data_dir + "c_test.parquet", index=False)
+
+for sc in train_sc:
+    src = os.path.join(data_dir + "scenarios_imgs/", sc)
+    dst = os.path.join(data_dir + "scenarios_imgs/split/train/", sc)
+    if os.path.exists(src):
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+        
+    print(f"Copied training scenario: {sc}")
+
+for sc in val_sc:
+    src = os.path.join(data_dir + "scenarios_imgs/", sc)
+    dst = os.path.join(data_dir + "scenarios_imgs/split/validation/", sc)
+    if os.path.exists(src):
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+        
+    print(f"Copied validation scenario: {sc}")
+
+for sc in test_sc:
+    src = os.path.join(data_dir + "scenarios_imgs/", sc)
+    dst = os.path.join(data_dir + "scenarios_imgs/split/test/", sc)
+    if os.path.exists(src):
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+        
+    print(f"Copied test scenario: {sc}")
