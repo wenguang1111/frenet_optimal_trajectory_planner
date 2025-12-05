c6b91b877120124387ba40e1c771233a0598a8db
Kareem Mohamed <karem.mohamed@dlr.de>
2025-12-05 01:31:10 +0100

    updated sbatch scripts

--- CVAE/cvae/sbatch/run_planner_script_cvae.sh
+++ CVAE/cvae/sbatch/run_planner_script_cvae.sh
@@ -5,9 +5,9 @@
 #SBATCH -e cvae/log/errors_cvae.err
 #SBATCH -D /dss/dsshome1/07/di97xub/frenet_optimal_trajectory_planner/CVAE
 #SBATCH --clusters=hpda2
-#SBATCH --partition=hpda2_compute_gpu
+#SBATCH --partition=hpda2_compute
 #SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1
+
 #SBATCH --mem=64gb
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=none
@@ -22,4 +22,4 @@ source /dss/dsshome1/07/di97xub/.cache/pypoetry/virtualenvs/cvae-3j1Yb4w3-py3.10
 
 cd /dss/dsshome1/07/di97xub/frenet_optimal_trajectory_planner/CVAE/commonroad-reactive-planner
 
-python3 run_planner.py --cvae
+python3 run_planner.py --cvae --mode test
